93528a37d8780a2ac911a8ad2bedb2943046187c
Henrique <henrique.krastins@gmail.com>
2023-02-09 20:38:59 -0300

    feat: login page background image size

--- frontend/src/layouts/Background.js
+++ frontend/src/layouts/Background.js
@@ -8,7 +8,7 @@ export const Background = styled.div`
   background: ${(props) =>
     props.colors ? 'none' : `url(${'https://wallpapercave.com/wp/wp10519849.jpg'}) no-repeat fixed center`};
   background-color: ${(props) => (props.colors ? props.colors.hex3 : '#66527B')};
-  background-size: contain;
+  background-size: 100% auto;
   background-repeat: repeat;
 
   z-index: -1;
